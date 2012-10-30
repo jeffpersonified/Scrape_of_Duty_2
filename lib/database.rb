7444062f668d72753364b52b7ff47966ec435e90
Daniela Grossmann <daniela.grossmann@gmail.com>
2012-10-30 14:28:06 -0700

    DB changes on to_database, return_id and some delete methods

--- lib/database.rb
+++ lib/database.rb
@@ -1,37 +1,38 @@
 require 'sqlite3'
 
 class CraigsDatabase
-  attr_reader :db_name
+  attr_accessor :db_name, :to_database
 
     DB_NAME = './craigs_list.db'
-  # def initialize
-  #    @db_name = 'craigs_list.db'
-  #  end
+  # def initialize(db_name = DB_NAME)
+  #      @db_name = db_name
+  #    end
 
-  def self.to_database(table, object, foreign_key_id = nil)
-    case table
-    when 'users'
-      insert_string = "INSERT INTO #{table} (name, password, username, email)
+  def self.to_database(object)
+    case object.class
+    when User
+      insert_string = "INSERT INTO users (name, password, username, email)
                       VALUES ('#{object.name}', '#{object.password}', '#{object.username}', '#{object.email}');"
-
-    when 'posts'
-      insert_string = "INSERT INTO #{table} (search_id, posted_at, title, price, neighborhood, post_url, content)
-                      VALUES ('#{foreign_key_id}', '#{object.posted_at}', '#{object.title}', '#{object.price}',
+    when Post
+      insert_string = "INSERT INTO posts (search_id, posted_at, title, price, neighborhood, post_url, content)
+                      VALUES ('#{return_id(object.search_url, 1)}', '#{object.posted_at}', '#{object.title}', '#{object.price}',
                       '#{object.neighborhood}', '#{object.post_url}', '#{object.content}');"
-    when 'searches'
-      insert_string = "INSERT INTO #{table} (search_url, category, region, min_price, max_price, keyword, user_id)
+    when Search
+      insert_string = "INSERT INTO searches (search_url, category, region, min_price, max_price, keyword, user_id)
                       VALUES ('#{object.search_url}', '#{object.category}', '#{object.region}',
-                      '#{object.min_price}', '#{object.max_price}', '#{object.keyword}', '#{foreign_key_id}');" # TODO: foreign key
+                      '#{object.min_price}', '#{object.max_price}', '#{object.keyword}', '#{return_id(object.username, 0)}');"
     end
     db_handler(insert_string)
   end
 
   def self.from_database(table)
+    # returns nested arrays
     reader_string = "SELECT * FROM #{table};"
     db_handler(reader_string)
   end
 
   def self.get_items(table, *items)
+    # returns nested arrays
     finder_string = "SELECT "
     (items.length - 1).times do |i|
       finder_string += "#{items[i]}, "
@@ -40,12 +41,42 @@ class CraigsDatabase
     db_handler(finder_string)
   end
 
+  def self.return_id(search_param, case_no)
+    case case_no
+    when 0
+      finder_string = "SELECT id FROM users WHERE username = #{search_param};"
+    when 1
+      finder_string = "SELECT id FROM searches WHERE url = #{search_param};"
+    end
+    db_handler(finder_string)
+  end
+
+
+  def self.find_item(search_word, search_class, table)
+    finder_string = "SEARCH * FROM #{table} WHERE #{search_class} = #{search_word};"
+    db_handler(finder_string)
+  end
+
+  def self.delete_content_from_table(table)
+    delete_string = "DELETE FROM #{table};"
+    db_handler(delete_string)
+  end
+
+  def self.delete_row_with_id(table, id)
+    delete_string = "DELETE FROM #{table} WHERE id = #{id};"
+    db_handler(delete_string)
+  end
+
   def self.db_handler(string)
     db = SQLite3::Database.open(DB_NAME)
     result = db.execute(string)
     db.close
     result
   end
+
+  # def self.connection
+  #    @connection ||= SQLite3::Database.open(@db_name)
+  #  end
 
 end
 
